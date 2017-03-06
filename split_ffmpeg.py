15bb1abe436f47787edabebe73fd6a2cfe02caa9
Matt Wilkie <maphew@gmail.com>
2017-03-05 21:17:35 -0800

    Name files by chapter name (courtesy of @clifgriffin)
    _"It outputs into a new directory with the name of the input file (minus
    extension). It also prefixes each chapter name with "1 - ", "2 - ", etc
    in case there are chapters with the same name."_
    
    http://stackoverflow.com/a/41309956/14420

--- split_ffmpeg.py
+++ split_ffmpeg.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
 import os
 import re
+import pprint
+import sys
 import subprocess as sp
+from os.path import basename
 from subprocess import *
 from optparse import OptionParser
 
@@ -8,20 +12,49 @@ def parseChapters(filename):
   chapters = []
   command = [ "ffmpeg", '-i', filename]
   output = ""
+  m = None
+  title = None
+  chapter_match = None
   try:
-    # ffmpeg requires an output file and so it errors 
-    # when it does not get one so we need to capture stderr, 
+    # ffmpeg requires an output file and so it errors
+    # when it does not get one so we need to capture stderr,
     # not stdout.
     output = sp.check_output(command, stderr=sp.STDOUT, universal_newlines=True)
   except CalledProcessError, e:
-    output = e.output 
+    output = e.output
+
+  num = 1
 
   for line in iter(output.splitlines()):
-    m = re.match(r".*Chapter #(\d+:\d+): start (\d+\.\d+), end (\d+\.\d+).*", line)
-    num = 0 
+    x = re.match(r".*title.*: (.*)", line)
+    print "x:"
+    pprint.pprint(x)
+
+    print "title:"
+    pprint.pprint(title)
+
+    if x == None:
+      m1 = re.match(r".*Chapter #(\d+:\d+): start (\d+\.\d+), end (\d+\.\d+).*", line)
+      title = None
+    else:
+      title = x.group(1)
+
+    if m1 != None:
+      chapter_match = m1
+
+    print "chapter_match:"
+    pprint.pprint(chapter_match)
+
+    if title != None and chapter_match != None:
+      m = chapter_match
+      pprint.pprint(title)
+    else:
+      m = None
+
     if m != None:
-      chapters.append({ "name": m.group(1), "start": m.group(2), "end": m.group(3)})
+      chapters.append({ "name": `num` + " - " + title, "start": m.group(2), "end": m.group(3)})
       num += 1
+
   return chapters
 
 def getChapters():
@@ -32,9 +65,16 @@ def getChapters():
     parser.error('Filename required')
   chapters = parseChapters(options.infile)
   fbase, fext = os.path.splitext(options.infile)
+  path, file = os.path.split(options.infile)
+  newdir, fext = os.path.splitext( basename(options.infile) )
+
+  os.mkdir(path + "/" + newdir)
+
   for chap in chapters:
+    chap['name'] = chap['name'].replace('/',':')
+    chap['name'] = chap['name'].replace("'","\'")
     print "start:" +  chap['start']
-    chap['outfile'] = fbase + "-ch-"+ chap['name'] + fext
+    chap['outfile'] = path + "/" + newdir + "/" + re.sub("[^-a-zA-Z0-9_.():' ]+", '', chap['name']) + fext
     chap['origfile'] = options.infile
     print chap['outfile']
   return chapters
@@ -52,7 +92,7 @@ def convertChapters(chapters):
         chap['outfile']]
     output = ""
     try:
-      # ffmpeg requires an output file and so it errors 
+      # ffmpeg requires an output file and so it errors
       # when it does not get one
       output = sp.check_output(command, stderr=sp.STDOUT, universal_newlines=True)
     except CalledProcessError, e:
